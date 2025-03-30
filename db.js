27ed81f3ca4fef2617859779dd51d263e307fc90
luhleet <74789293+luhleet@users.noreply.github.com>
2025-03-30 18:15:15 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,3 +5,5 @@ const SUPABASE_URL = "https://ssethvlmwzdoainknztb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzZXRodmxtd3pkb2FpbmtuenRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0OTEwMzksImV4cCI6MjA1ODA2NzAzOX0.pyI4BkggUm9DnAIXaeAqyN99ymMJ98s-PnGMyMBqZu4";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
